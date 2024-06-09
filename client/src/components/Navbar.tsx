f3aa805a5367cd88442ca5e87d4daaf25d323877
Håkon Borhaug <haakon_borhaug@hotmail.com>
2024-06-09 17:18:14 +0200

    misc

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
@@ -15,23 +15,35 @@ function Navbar() {
         </Nav>
         <Button
           style={{
-            height: "3rem",
             width: "3rem",
-            display: "flex",
-            padding: "0.5rem",
-            alignItems: "center",
+            height: "3rem",
+            position: "relative",
+            alignContent: "center",
           }}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="20"
+            height="20"
             fill="currentColor"
             className="bi bi-cart"
             viewBox="0 0 16 16"
           >
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
           </svg>
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-tiems-center"
+            style={{
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
         </Button>
       </Container>
     </NavbarBs>
